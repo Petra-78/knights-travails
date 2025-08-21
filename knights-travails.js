2e6f5f68a3adebe695daa08998bf0843e5f04714
Petra-78 <partos.petra.0305@gmail.com>
2025-08-21 16:37:44 +0200

    add bfs traversal framework

--- knights-travails.js
+++ knights-travails.js
@@ -19,8 +19,36 @@ function getPossibleMoves([i, j]) {
       possibleMoves.push([ix, jy]);
     }
   }
-  console.log(possibleMoves);
+  console.log(`the possible moves are: ${possibleMoves}`);
   return possibleMoves;
 }
 
 getPossibleMoves([3, 3]);
+
+function knightMoves([i, j]) {
+  debugger;
+  let q = [[i, j]];
+  let shortestPath = [];
+  let visited = Array.from({ length: 8 }, () => Array(8).fill(false));
+  console.log(visited);
+
+  while (q.length > 0) {
+    let current = q.shift();
+    let ci = current[0];
+    let cj = current[1];
+
+    if (visited[ci][cj] !== true) {
+      const moves = getPossibleMoves(current);
+      for (let [x, y] of moves) {
+        if (!visited[x][y]) {
+          q.push([x, y]);
+        }
+      }
+      visited[ci][cj] = true;
+    }
+
+    console.log(`queue is : ${q}`);
+  }
+}
+
+knightMoves([0, 0]);
